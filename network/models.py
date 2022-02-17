517889b471110810dbcbac50574b25f1a25d4ff8
mateuszksiazek <mateuszksiazek@LAPTOP-13HOSNLJ.localdomain>
2022-02-17 23:16:14 +0100

    Add following count and followers count to User model and like count to Post model

--- network/models.py
+++ network/models.py
@@ -3,12 +3,15 @@ from django.db import models
 
 
 class User(AbstractUser):
-    following = models.ManyToManyField('self', on_delete=models.CASCADE, related_name='followers')
+    following = models.ManyToManyField('self', related_name='followers')
+    following_count = models.IntegerField(default=0, blank=True)
+    followers_count = models.IntegerField(default=0, blank=True)
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     content = models.TextField(max_length=2048)
-    likers = models.ManyToManyField(User, on_delete=models.CASCADE, related_name='liked_posts')
+    likers = models.ManyToManyField(User, related_name='liked_posts')
+    likes = models.IntegerField(default=0, blank=True)
 
 
 
